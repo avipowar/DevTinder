afd472943a6b67de0d3055311feddf7050cfb06a
Avinash Powar <powaravinash06@gmail.com>
2025-12-09 09:39:39 +0530

    fix issu

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // connect database
 const connectDB = (async = async () => {
   await mongoose.connect(
-    "mongodb+srv://powaravinash06_db_user:NCnXKvocNyayiIjW@namsatedev.qnzwemy.mongodb.net/"
+    "mongodb+srv://powaravinash06_db_user:NCnXKvocNyayiIjW@namsatedev.qnzwemy.mongodb.net/devTinder"
   );
 });
 
