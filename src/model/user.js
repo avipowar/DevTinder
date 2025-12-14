569cb6aad0763b9f1752c145adcdbef403087f07
Avinash Powar <powaravinash06@gmail.com>
2025-12-14 08:51:22 +0530

    add validate function and timestamp

--- src/model/user.js
+++ src/model/user.js
@@ -1,44 +1,57 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 4,
-    maxLength: 10,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 10,
+    },
+    lastName: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender Data Is Not Valid");
+        }
+      },
+      lowercase: true,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    passWord: {
+      type: String,
+      required: true,
+    },
+    photoUrl: {
+      type: String,
+    },
+    about: {
+      type: String,
+      default: "This is about the new user",
+    },
+    skills: [
+      {
+        type: String,
+        lowercase: true,
+        trim: true,
+      },
+    ],
   },
-  lastName: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  passWord: {
-    type: String,
-    required: true,
-  },
-  photoUrl: {
-    type: String,
-  },
-  about: {
-    type: String,
-    default: "This is about the new user",
-  },
-  skills: {
-    type: [String],
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
