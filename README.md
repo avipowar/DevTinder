4d502f8a31053dba2be004f2a0ef5af909c682c6
Avinash Powar <powaravinash06@gmail.com>
2025-12-29 11:34:31 +0530

    make app the accepted req show api

--- README.md
+++ README.md
@@ -164,7 +164,11 @@
             - is coming req is in my database or not
             - or my status is interested or not
 
-    - Create get APIs( show all the receive req getting from user ) :  - GET /user/request/receives
+    - Create get APIs( show all the receive/pending req getting from user ) :  - GET /user/request/receives
 
         - ref : tells the mongoose => this object id => which collection belongs to
         - populate : fetches the reference document => and replace with the objectId
+
+    - Create get APIs( show all the accepted ) :  - GET /user/connections
+        - map function
+        - $or [{},{}]
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,15 +4,17 @@ const ConnectionRequest = require("../model/connectionRequest");
 const userRouter = express.Router();
 const mongoose = require("mongoose");
 
+const USER_SAFE_DATA = "firstName lastName age about";
+
 userRouter.get("/user/request/receives", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
-    console.log(typeof loggedInUser._id);
+    // console.log(typeof loggedInUser._id);
 
     const connectionRequest = await ConnectionRequest.find({
       toUserId: new mongoose.Types.ObjectId(loggedInUser._id),
       status: "interested",
-    }).populate("formUserId", "firstName lastName age about");
+    }).populate("formUserId", USER_SAFE_DATA);
 
     res.json({
       message: "Got All The Request successfully",
@@ -20,6 +22,32 @@ userRouter.get("/user/request/receives", userAuth, async (req, res) => {
     });
   } catch (error) {
     res.status(404).send("ERROR: " + error.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser, status: "accepted" },
+        { formUserId: loggedInUser, status: "accepted" },
+      ],
+    })
+      .populate("formUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connectionRequest.map((row) => {
+      if (row.formUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.formUserId;
+    });
+
+    res.json({ data });
+  } catch (error) {
+    res.status(404).send("ERROR:" + error.message);
   }
 });
 
